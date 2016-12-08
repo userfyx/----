2162745607960fde087873febe194e44c31ce183
userfyx <2450907310@qq.com>
2016-12-08 22:16:40 +0800

    12月8日
    12月8日push

---
+++ 12月8日/push/PushTestReceiver.java
@@ -0,0 +1,57 @@
+package com.hhzmy.push;
+
+import android.content.Context;
+
+import com.baidu.android.pushservice.PushMessageReceiver;
+
+import java.util.List;
+
+/**
+ * Created by a on 2016/12/8.
+ */
+/*
+*1 作用
+*2 范永兴
+*3 2016/11/24
+*/
+public class PushTestReceiver extends PushMessageReceiver {
+    @Override
+    public void onBind(Context context, int i, String s, String s1, String s2, String s3) {
+
+    }
+
+    @Override
+    public void onUnbind(Context context, int i, String s) {
+
+    }
+
+    @Override
+    public void onSetTags(Context context, int i, List<String> list, List<String> list1, String s) {
+
+    }
+
+    @Override
+    public void onDelTags(Context context, int i, List<String> list, List<String> list1, String s) {
+
+    }
+
+    @Override
+    public void onListTags(Context context, int i, List<String> list, String s) {
+
+    }
+
+    @Override
+    public void onMessage(Context context, String s, String s1) {
+
+    }
+
+    @Override
+    public void onNotificationClicked(Context context, String s, String s1, String s2) {
+
+    }
+
+    @Override
+    public void onNotificationArrived(Context context, String s, String s1, String s2) {
+
+    }
+}
