2c42b355846c39f536ec61e9b57495982c578d92
userfyx <2450907310@qq.com>
2016-12-27 22:17:50 +0800

    12月27日限定符
    限定符

---
+++ 限定符Dome/src/main/java/com/dome/LeftFragment.java
@@ -0,0 +1,28 @@
+package com.dome;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LeftFragment extends Fragment {
+
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_left, container, false);
+//        view.setBackgroundColor(Color.GREEN);
+
+        return view;
+    }
+
+}
---
+++ 限定符Dome/src/main/java/com/dome/RightFragment.java
@@ -0,0 +1,23 @@
+package com.dome;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class RightFragment extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_right, container, false);
+//        view.setBackgroundColor(Color.WHITE);
+        return view;
+    }
+
+}
